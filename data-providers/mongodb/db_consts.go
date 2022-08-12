e3e8b7c2f3ad608d0941542ad83dbd632fda9399
valerijvorobev <vorobjevvalera@gmail.com>
2022-08-12 18:06:56 +0300

    add db consts

---
+++ data-providers/mongodb/db_consts.go
@@ -0,0 +1,11 @@
+package mongodb
+
+// Collections names
+const (
+	UserCollection     = "users"      // Collection for store users
+	CategoryCollection = "categories" // Collection for course categories
+	StageCollection    = "stages"     // Collection store stages for courses
+	CourseCollection   = "courses"    // Collection store courses
+)
+
+const Db = "opencourse" // Database name
